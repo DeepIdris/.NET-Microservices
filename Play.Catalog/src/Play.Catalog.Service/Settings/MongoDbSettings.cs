b498fdbcbca6b27cc2d7fb2dd7b5dc99fbac5684
Idris Oyebode <oyebodeidris@gmail.com>
2022-05-21 01:07:56 +0100

    Implemented Dependency Injection

---
+++ Play.Catalog/src/Play.Catalog.Service/Settings/MongoDbSettings.cs
@@ -0,0 +1,13 @@
+namespace Play.Catalog.Service.Settings
+{
+    public class MongoDbSettings
+    {
+        public string Host { get; init; }
+        public int Port { get; init; }  
+        public string User { get; set; }
+        public string Password { get; set; }
+
+        public string ConnectionString => $"mongodb://{Host}:{Port}";
+
+    }
+}
