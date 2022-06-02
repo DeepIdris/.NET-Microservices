717f55733e406b8e08a1449a4c4a3e49600d02d2
Idris Oyebode <oyebodeidris@gmail.com>
2022-06-02 23:42:21 +0100

    Removed Inter-service synchonous communication

--- Play.Common/src/Play.Common/MassTransit/Extensions.cs
+++ Play.Common/src/Play.Common/MassTransit/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using MassTransit;
 using Microsoft.Extensions.Configuration;
@@ -21,6 +22,10 @@ namespace Play.Common.MassTransit
                     var rabbitMQSettings =  configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
                     configurator.Host(rabbitMQSettings.Host);
                     configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+                    configurator.UseMessageRetry(retryConfigurator =>
+                    {
+                        retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
+                    });
                 });
             });
 
